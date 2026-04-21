3fbc39a87e915c68f88f5c9852805ff43dc59dc5
Bardh Gashi <bardhgjeki@gmail.com>
2026-04-21 09:28:50 +0200

    git remote add origin https://github.com/bg20033/supertasku.git

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FamilySetupView } from './features/family-setup/family-setup.component';
+import { HealthFormViewView } from './features/health-form-view/health-form.component';
+import { LandingPageView } from './features/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageView,
+    title: 'Gesundheitsdeklaration starten',
+  },
+  {
+    path: 'family',
+    component: FamilySetupView,
+    title: 'Familie erfassen',
+  },
+  {
+    path: 'health',
+    component: HealthFormViewView,
+    title: 'Gesundheitsdeklaration',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
